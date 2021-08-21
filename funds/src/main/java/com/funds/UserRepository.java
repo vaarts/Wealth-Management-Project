30b2f15b3f86a94fb3ad809aa852a386f518606f
Vipul Aggarwal <74702064+vaarts@users.noreply.github.com>
2021-08-21 18:05:07 +0530

    Add files via upload

---
+++ funds/src/main/java/com/funds/UserRepository.java
@@ -0,0 +1,11 @@
+package com.funds;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer>{
+	
+	User findByUserEmailAndUserPassword(String userEmail, String userPassword);
+
+}
